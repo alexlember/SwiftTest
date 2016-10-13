48b0962647ec6a7cfa9dfb14dd78b82994575bb4
Алексей Лемберский <alexlember@gmail.com>
2016-10-13 22:58:11 +0300

    Initial Commit

---
+++ SwiftViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftViewTest
+//
+//  Created by Алексей Лемберский on 13.10.16.
+//  Copyright © 2016 Алексей Лемберский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
